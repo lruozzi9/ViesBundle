f96c35811d8369b54ec48bd76214be0d1f76fa5b
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-11-19 15:13:23 +0100

    Do not allow to pass vat number with "0"

--- src/Validator/Constraint/VatNumberValidator.php
+++ src/Validator/Constraint/VatNumberValidator.php
@@ -29,7 +29,7 @@ final class VatNumberValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint): void
     {
-        if (empty($value)) {
+        if ($value === '') {
             return;
         }
 
